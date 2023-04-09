91a42fc430d04e9ab069c542b06a9719fac9ea8d
tonyekim <otoobongekim@gmail.com>
2023-04-09 21:33:48 +0100

    start script

--- markdown2html.py
+++ markdown2html.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-''' Write a script markdown2html.py that takes an argument 2 strings:
 
-    First argument is the name of the Markdown file
-    Second argument is the output file name
-'''
-
+"""
+Markdown script using python.
+"""
 import sys
 import os.path
 import re
